cb750e19fdae0c426415e7797bde2e37397de2cf
mberrueta <matiasberrueta@gmail.com>
2022-06-22 09:59:45 -0300

    removed old tests

--- test/factories/customers.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :customer do
-    name { Faker::Name.name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Internet.email }
-  end
-end
